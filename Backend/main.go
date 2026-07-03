6405011120c35c1a367a36cc46a59e43a7e0ddcf
Chinmay Shrivastav <chinmayshrivastav556@gmail.com>
2026-07-03 14:18:29 +0530

    Implemented Get method and Error returner function

--- Backend/main.go
+++ Backend/main.go
@@ -2,10 +2,56 @@ package main
 
 import(
 	"net/http"
+	"encoding/json"
 )
 
-func taskHandler(w http.ResponseWriter, r *http.Request){
+var db map[int] Tasks
+var nextID=1
 
+type Tasks struct{
+	ID int `json:"id"`
+	Task string `json:"task"`
+	Completed bool `json:"completed"`
+}
+
+type TaskList struct{
+	Message string `json:"message"`
+	Tasks map[int]Tasks `json:"tasks"`
+}
+
+type errResp struct{
+	Message string `json:"message"`
+}
+
+func returnError(w http.ResponseWriter, errMsg string, code int){
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	enc:=json.NewEncoder(w)
+	enc.SetIndent("", "    ")
+	resp:=errResp{
+		Message: errMsg,
+	}
+	err:=enc.Encode(resp)
+	if (err!=nil){
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+}
+
+func taskHandler(w http.ResponseWriter, r *http.Request){
+	if (r.Method==http.MethodGet){
+		w.Header().Set("Content-Type", "application/json")
+		resp:=TaskList{
+			Message:"Request Successful",
+			Tasks: db,
+		}
+		encoder:=json.NewEncoder(w)
+		encoder.SetIndent("", "    ")
+		err:=encoder.Encode(resp)
+		if (err!=nil){
+			returnError(w, "Internal Server Error", http.StatusInternalServerError)
+		}
+		return
+	}
 }
 
 func main(){
